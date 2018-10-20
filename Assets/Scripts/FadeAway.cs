8081fba9d714aa7d4d03f8098b71d7938c459b6c
Lukesteral <44324251+Lukesteral@users.noreply.github.com>
2018-10-20 15:32:12 -0600

    Added stars, rotating globe, title

---
+++ Assets/Scripts/FadeAway.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(CountDownTimer))]
+public class FadeAway : MonoBehaviour
+{
+    //
+    private CountDownTimer countDownTimer;
+    private Text textUI;
+    //Number of seconds before the text completely fades away.
+
+    private int startSeconds = 5;
+    private bool fading = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        textUI = GetComponent<Text>();
+        countDownTimer = GetComponent<CountDownTimer>();
+
+        StartFading(startSeconds);
+	}
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(fading)
+        {
+            //GEt the time remaining as a float betweeen 0.0 and 1.0
+            float alphaRemaining = countDownTimer.GetProportionTimeRemaining();
+            print(alphaRemaining);
+            Color c = textUI.material.color;
+
+            //set alpha to time remaining percentgae
+            //s0 1.0 means full text color .5 means half transparent, etc.
+            c.a = alphaRemaining;
+            textUI.material.color = c;
+            //stop fading one very small number
+            if(alphaRemaining < 0.01)
+            {
+                fading = false;
+            }
+        }
+
+	}
+
+    private void StartFading(int timerTotal)
+    {
+        countDownTimer.ResetTimer(timerTotal);
+
+        fading = true;
+    }
+}
