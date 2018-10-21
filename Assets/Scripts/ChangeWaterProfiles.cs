c7ea1e5168e94a5eaf501acc47aceeca3a430317
Kat Moore <katherinemoore333@gmail.com>
2018-10-20 22:38:12 -0600

    Water stuff
    - Changes colors, background once user is under the water
    - Water moves up
    - Collider disappears after couple seconds

---
+++ Assets/Scripts/ChangeWaterProfiles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class ChangeWaterProfiles : MonoBehaviour {
+
+    public PostProcessingProfile normal, underwater;
+
+    private PostProcessingBehaviour cameraImage;
+
+	// Use this for initialization
+	void Start () {
+        cameraImage = FindObjectOfType<PostProcessingBehaviour>();
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            cameraImage.profile = underwater;
+            Debug.Log("Underwater");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            cameraImage.profile = normal;
+        }
+    }
+}
--- Assets/Scripts/WaterColliderController.cs
+++ Assets/Scripts/WaterColliderController.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterController : MonoBehaviour {
+public class WaterColliderController : MonoBehaviour {
 
-    // Target marker
-    //public Transform target;
-
-    // Speed in units per sec
     public float speed;
-    Vector3 target = new Vector3(-169, 5, -12);
-	// Update is called once per frame
-	void Update () {
+    Vector3 target = new Vector3(-120, 4, -11);
+    // Update is called once per frame
+    void Update()
+    {
         float step = speed * Time.deltaTime;
 
         transform.position = Vector3.MoveTowards(transform.position, target, step);
-	}
+    }
 }
+
+
